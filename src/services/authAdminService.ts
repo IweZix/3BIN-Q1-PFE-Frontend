c725315ec02578709c8c5f4a6a96a197c2ca66ff
Alexandre GarciaPerez <you@example.com>
2024-12-04 16:10:12 +0100

    init branch

--- src/services/authAdminService.ts
+++ src/services/authAdminService.ts
@@ -3,10 +3,23 @@ import axios from 'axios';
 const API_URL = 'http://localhost:3000/authAdmin';
 
 export const loginAdmin = async (email: string, password: string) => {
-    const response = await axios.post(`${API_URL}/login-admin`, { email, password });
+    const response = await axios.post(${API_URL}/login-admin, { email, password });
     return response.data;
 };
 export const adminVerif = async (token: string) => {
-    const response = await axios.post(`${API_URL}/verify-admin`, { token });
+    const response = await axios.post(${API_URL}/verify-admin, { token });
     return response.data;
+};
+export const registerAdmin = async (email: string, password: string) => {
+    try {
+        const response = await axios.post(${API_URL}/register-admin, { email, password });
+        return response.data; // Retourne les données de l'API
+    } catch (error) {
+        // Gestion des erreurs
+        if (error.response) {
+            throw new Error(error.response.data.message || 'Erreur lors de l'enregistrement de l'administrateur.');
+        } else {
+            throw new Error('Impossible de se connecter au serveur.');
+        }
+    }
 };
